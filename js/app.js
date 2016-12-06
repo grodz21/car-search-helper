9e6c5b4b66492fb50dcd30556c4247203c8a84ec
Gabriel <gaby4b11@gmail.com>
2016-12-06 14:25:05 -0500

    Added state/year of vehicle function
    Added function so that whenever the user selects state of the car or
    year, they automatically pull the brand of cars available.
    
    *Need to populate them in a separate options*

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 		.fail(errorHandler);
 
 		function successHandler(data){
-			;
+			console.log(data);
 		};
 
 
@@ -33,7 +33,37 @@ $(document).ready(function(){
 	});
 
 
-	$
+
+	// This function gets car makes by condition of car / year and populates it in the options.
+	$("#yearOfCar, #stateOfCar").change(function() {
+  		// console.log(document.getElementById('yearOfCar').value);
+
+  		var stateOfVehicle = $('#stateOfCar').val();
+		var modelYear = Number($('#yearOfCar').val());
+
+  		$.ajax({
+			url: "https://api.edmunds.com/api/vehicle/v2/makes?state=" + stateOfVehicle + "&year=" + modelYear + 
+			"&view=basic&fmt=json&api_key=" + apiKey ,
+
+			method: "GET",
+
+		})
+		.done(successHandler)
+		.fail(errorHandler);
+
+		function successHandler(data){
+			console.log(data);
+		};
+
+
+		function errorHandler(error){
+			console.log(error);
+		};
+
+
+		
+		
+	});
 
 
 
