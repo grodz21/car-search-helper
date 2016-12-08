fd71e54bf465ab0dd5a6a3a3b06f9caf22b14bba
Gabriel <gaby4b11@gmail.com>
2016-12-08 12:51:48 -0500

    Ajax call to retrieve the model list array added
    Added Ajax call to retrieve the model list array of each car
    manufacturer depending on car make selection. For now the output is in
    the console.log

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){
+
 	
 	var apiKey = "xaq2gcmpz7k5kt84d6wvwdpf";
 
+	
 	
 
 
@@ -36,10 +37,11 @@ $(document).ready(function(){
 
 	// This function gets car makes by condition of car / year and populates it in the options.
 	$("#yearOfCar, #stateOfCar").change(function() {
-  		// console.log(document.getElementById('yearOfCar').value);
+  		
 
   		var stateOfVehicle = $('#stateOfCar').val();
 		var modelYear = Number($('#yearOfCar').val());
+
 
   		$.ajax({
 			url: "https://api.edmunds.com/api/vehicle/v2/makes?state=" + stateOfVehicle + "&year=" + modelYear + 
@@ -53,6 +55,54 @@ $(document).ready(function(){
 
 		function successHandler(data){
 			console.log(data);
+
+			var nameList = data.makes.map(function(make) {
+  				return make.niceName;
+  			});
+
+			console.log(nameList);
+			/*This clears the Make selection before appending new makes
+			based in changes between condition and year.*/
+			// $("#carMake").html("");
+
+			/*This appends the list of car makes to the make dropdown*/
+			for(i=0;nameList[i].length;i++){
+			$("#carMake").append("<option>" + nameList[i] + "</option>");
+			}
+
+			// var modelList = data.makes.map(function(model) {
+	  // 				return models.niceName;
+	  // 			});
+
+
+		//Maintenance recalls ajax call 
+		$.ajax({
+
+			url: "https://api.edmunds.com/v1/api/maintenance/recall/200002038" + "?fmt=json&api_key=" + apiKey,
+
+			method: "GET",
+
+		})
+		.done(successRecallHandler)
+		.fail(errorRecallHandler);
+
+		function successRecallHandler(data){
+			console.log(data);
+		}
+
+		function errorRecallHandler(error){
+			console.log(error);
+		}
+
+		// Throw new error
+		if (data.makesCount === 0){
+			alert("No cars available");
+		}
+		else {
+		/*Here goes function that gets the ".makes["0"].name" and 
+		prints it to "carMake" option selection. Render list names*/
+			console.log(data.makes[0].name);
+			}
 		};
 
 
@@ -61,9 +111,60 @@ $(document).ready(function(){
 		};
 
 
-		
-		
 	});
+
+
+		// This handles the updating of the car models list
+		$("#carMake").change(function() {
+			var selectedState = $( "#stateOfCar option:selected" ).text();
+
+			var selectedYear = $( "#yearOfCar" ).val();
+
+			var selectedCarMake = $( "#carMake option:selected" ).text();
+
+			
+			
+			
+			$.ajax({
+				url: "https://api.edmunds.com/api/vehicle/v2/" + selectedCarMake + 
+				"?state=" + selectedState + "&year=" + selectedYear + "&view=basic&fmt=json&api_key=" + apiKey,
+
+				method: "GET",
+
+				})
+				.done(successRecallHandler)
+				.fail(errorRecallHandler);
+
+				function successRecallHandler(data){
+					console.log(data);
+				}
+
+				function errorRecallHandler(error){
+					console.log(error);
+				}
+
+				
+
+			});
+
+
+
+				// /*This clears the Make selection before appending new makes
+				// based in changes between condition and year.*/
+				// $("#carModel").html("");
+
+				// /*This appends the list of car makes to the make dropdown*/
+				// for(i=0;makeList[i].length;i++){
+				// $("#carModel").append("<option>" + makeList[i] + "</option>");
+				// }
+
+		
+
+
+
+
+
+
 
 
 
@@ -95,7 +196,5 @@ $(document).ready(function(){
 
 
 
-	
 
-})
 
