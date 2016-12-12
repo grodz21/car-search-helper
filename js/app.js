fcd0e0930497f6a9540e8c1c32db5a33d9b50445
Gabriel <gaby4b11@gmail.com>
2016-12-12 18:55:35 -0500

    Added ajax calls depending on user selection
    Added an options selector for the user to look up for vehicle service
    bulletins or vehicle recalls.

--- js/app.js
+++ js/app.js
@@ -14,30 +14,73 @@
 		console.log($("#carModel").val());
 		console.log(carToIdMapping);
 		var carModel = $("#carModel").val();
-		console.log(carToIdMapping[carModel]);
+		console.log(carToIdMapping[carModel]); //Console logs model id by number. Ex:200701415
+
+		if($("#infoDisplayOption").val() === "1"){
+			// This one submits the get request for Vehicle Recalls
 
 
+			$.ajax({
+			url: "https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=" + 
+					carToIdMapping[carModel] + "&fmt=json&api_key=" + apiKey ,
 
-	// https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=200437625&fmt=json&api_key=xaq2gcmpz7k5kt84d6wvwdpf
-	// 	$.ajax({
-	// 		url: "https://api.edmunds.com/api/vehicle/v2/makes?state=" + stateOfVehicle + "&year=" + modelYear + 
-	// 		"&view=basic&fmt=json&api_key=" + apiKey ,
+			method: "GET",
 
-	// 		method: "GET",
+			})
+			.done(function(data){
+			  console.log(data);
+			  // data[0]
 
-	// 	})
-	// 	.done(successHandler)
-	// 	.fail(errorHandler);
+			  if (data.recallHolder.length === 0) {
+			  		$(".contentWrapper").html("<p>No recalls found.</p>");
 
-	// 	function successHandler(data){
-	// 		console.log(data);
- //  		}
+				  }	else {
+				  		var templateToRender = $("#recall-template2").html();
+				 		Mustache.parse(templateToRender);
+				 		var rendered = Mustache.render(templateToRender, data);
+	  					$(".contentWrapper").html(rendered);
+			  }
 
- //  		function errorHandler(data){
+			 
+			})
+			.fail(function(error){
+			  console.log(error);
 
- //  		}
+			})
 
-		
+		}
+		else {
+			// This one submits the get request for Vehicle Service Bulletins
+
+
+			$.ajax({
+			url: "https://api.edmunds.com/v1/api/maintenance/servicebulletinrepository/findbymodelyearid?modelyearid=" + 
+					carToIdMapping[carModel] + "&fmt=json&api_key=" + apiKey ,
+
+			method: "GET",
+
+			})
+			.done(function(data){
+			  console.log(data);
+
+			  if (data.serviceBulletinHolder.length === 0) {
+			  		$(".contentWrapper").html("<p>No technical service bulletins found.</p>");
+
+				  }	else {
+				  		var tsbTemplateToRender = $("#serviceBulletin-template").html();
+				 		Mustache.parse(tsbTemplateToRender);
+				 		var renderedTsbTemplate = Mustache.render(tsbTemplateToRender, data);
+	  					$(".contentWrapper").html(renderedTsbTemplate);
+			  }
+			  
+			})
+
+			.fail(function(error){
+			  console.log(error);
+			})
+		}
+
+	
 	});
 
 
@@ -49,73 +92,76 @@
   		var stateOfVehicle = $('#stateOfCar').val();
 		var modelYear = Number($('#yearOfCar').val());
 
+		if (modelYear !== 0) {
 
-  		$.ajax({
-			url: "https://api.edmunds.com/api/vehicle/v2/makes?state=" + stateOfVehicle + "&year=" + modelYear + 
-			"&view=basic&fmt=json&api_key=" + apiKey ,
+				$.ajax({
+				url: "https://api.edmunds.com/api/vehicle/v2/makes?state=" + stateOfVehicle + "&year=" + modelYear + 
+				"&view=basic&fmt=json&api_key=" + apiKey ,
 
-			method: "GET",
+				method: "GET",
 
-		})
-		.done(successHandler)
-		.fail(errorHandler);
+			})
+			.done(successHandler)
+			.fail(errorHandler);
 
-		function successHandler(data){
-			console.log(data);
+			function successHandler(data){
+				console.log(data);
 
-			var nameList = data.makes.map(function(make) {
-  				return make.niceName;
-  			});
+				var nameList = data.makes.map(function(make) {
+	  				return make.niceName;
+	  			});
 
-			console.log(nameList);
-			/*This clears the Make selection before appending new makes
-			based in changes between condition and year.*/
-			// $("#carMake").html("");
+				console.log(nameList);
+				/*This clears the Make selection before appending new makes
+				based in changes between condition and year.*/
+				// $("#carMake").html("");
 
-			/*This appends the list of car makes to the make dropdown*/
-			for(var i = 0; i < nameList.length; i++){
-			$("#carMake").append("<option>" + nameList[i] + "</option>");
+				/*This appends the list of car makes to the make dropdown*/
+				for(var i = 0; i < nameList.length; i++){
+				$("#carMake").append("<option>" + nameList[i] + "</option>");
+				}
+
+				// var modelList = data.makes.map(function(model) {
+		  // 				return models.niceName;
+		  // 			});
+
+
+			//Maintenance recalls ajax call 
+			$.ajax({
+
+				url: "https://api.edmunds.com/v1/api/maintenance/recall/200002038" + "?fmt=json&api_key=" + apiKey,
+
+				method: "GET",
+
+			})
+			.done(successRecallHandler)
+			.fail(errorRecallHandler);
+
+			function successRecallHandler(data){
+				console.log(data);
 			}
 
-			// var modelList = data.makes.map(function(model) {
-	  // 				return models.niceName;
-	  // 			});
-
-
-		//Maintenance recalls ajax call 
-		$.ajax({
-
-			url: "https://api.edmunds.com/v1/api/maintenance/recall/200002038" + "?fmt=json&api_key=" + apiKey,
-
-			method: "GET",
-
-		})
-		.done(successRecallHandler)
-		.fail(errorRecallHandler);
-
-		function successRecallHandler(data){
-			console.log(data);
-		}
-
-		function errorRecallHandler(error){
-			console.log(error);
-		}
-
-		// Throw new error
-		if (data.makesCount === 0){
-			alert("No cars available");
-		}
-		else {
-		/*Here goes function that gets the ".makes["0"].name" and 
-		prints it to "carMake" option selection. Render list names*/
-			console.log(data.makes[0].name);
+			function errorRecallHandler(error){
+				console.log(error);
 			}
-		};
+
+			// Throw new error
+			if (data.makesCount === 0){
+				alert("No cars available");
+			}
+			else {
+			/*Here goes function that gets the ".makes["0"].name" and 
+			prints it to "carMake" option selection. Render list names*/
+				console.log(data.makes[0].name);
+				}
+			};
 
 
-		function errorHandler(error){
-			console.log(error);
-		};
+			function errorHandler(error){
+				console.log(error);
+			};
+		}
+  		
 
 
 	});
@@ -176,49 +222,6 @@
 
 
 
-				// /*This clears the Make selection before appending new makes
-				// based in changes between condition and year.*/
-				// $("#carModel").html("");
-
-				// /*This appends the list of car makes to the make dropdown*/
-				// for(i=0;makeList[i].length;i++){
-				// $("#carModel").append("<option>" + makeList[i] + "</option>");
-				// }
-
-		
-
-
-
-
-
-
-
-
-
-
-	// function getAllCars(stateOfCar, modelYear){
-
-	// 	$.ajax({
-	// 		url: "https://api.edmunds.com/api/vehicle/v2/makes?state=" + stateOfVehicle + "&year=" + modelYear + 
-	// 		"&view=basic&fmt=json&api_key=" + apiKey ,
-
-	// 		method: "GET",
-
-	// 	})
-	// 	.done(successHandler)
-	// 	.fail(errorHandler);
-
-	// 	function successHandler(data){
-	// 		console.log(data);
-	// 	};
-
-
-	// 	function errorHandler(error){
-	// 		console.log(error);
-	// 	};
-
-
-	// }
 
 
 
